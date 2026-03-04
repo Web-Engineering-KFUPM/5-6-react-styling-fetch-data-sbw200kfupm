21b0744a364e93f77b2030befd3037d0af542d3c
sbw200kfupm <s202337730@kfupm.edu.sa>
2026-03-04 12:13:40 +0300

    todo2

--- 5-6-react-styling-fetch-data/src/App.jsx
+++ 5-6-react-styling-fetch-data/src/App.jsx
@@ -204,6 +204,26 @@ export default function App() {
      ========================================================= */
   useEffect(() => {
     // TODO 2.1: Implement fetching users here (see lab instructions)
+    const fetchData = async () => {
+      try {
+            setLoading(true);
+            setError(null);
+            const response = await fetch('https://jsonplaceholder.typicode.com/users');
+            if (!response.ok) {
+               throw new Error("Failed to fetch users");
+            }
+            const data = await response.json();
+            setUsers(data);
+            setFilteredUsers(data);
+            setError(err.message);
+      } catch (error) {
+         setError(err.message);
+         console.error(error);
+      } finally {
+         setLoading(false);
+      }
+   };
+   fetchData();
   }, []);
 
   /* =========================================================
@@ -215,6 +235,14 @@ export default function App() {
      ========================================================= */
   useEffect(() => {
     // TODO 2.2: Implement filtering users here (see lab instructions)
+      if (searchTerm === "") {
+         setFilteredUsers(users);
+      } else {
+         const filtered = users.filter((user) =>
+            user.name.toLowerCase().includes(searchTerm.toLowerCase())
+         );
+         setFilteredUsers(filtered);
+      }
   }, [searchTerm, users]);
 
   // Modal handlers (already complete)
